c49cf6eb567008cb93a0a5ae78c0851cc2634d7e
Marcel Bührig <marcel.buehrig@zeix.com>
2016-10-14 15:22:38 +0200

    Next try

--- src/js/slyder.js
+++ src/js/slyder.js
@@ -93,7 +93,7 @@
         $curr.addClass(_slyder.options.current);
         _slyder.$el.css({
             height: _height+'px',
-            left: -1 * _slyder.$el.width()
+            'left': -1 * _slyder.$el.width()
         });
 
         _slyder.$el.trigger('moveContent');
@@ -115,7 +115,7 @@
         $curr.removeClass(_slyder.options.current);
         _slyder.$el.css({
             height: _height+'px',
-            left: 0
+            'left': 0
         });
 
         _slyder.$el.trigger('moveContent');
